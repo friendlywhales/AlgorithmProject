18aee3aaa9169dea0e8800d6683b43bb895a4338
monRay84 <sooeun@gmail.com>
2017-11-08 14:14:24 +0900

    homework start

---
+++ src/FindTravelPath.java
@@ -0,0 +1,31 @@
+
+public class FindTravelPath {
+
+	public static void main(String[] args) {
+		//여행지 총 수 
+		int TravelPathTotalCnt = 4;
+		
+		//경로당 소요비용 집합
+		
+		//경로당 소요시간 집합
+		
+		//최소 [소요비용 + 소요시간] 값
+		double minValue = 0;
+
+	}
+	
+	private double getPathValue(int totalPathCnt, int start, int end){
+		double result = 0;
+		
+		//소요시간 가중치 
+		double timeValue = 0.7;
+		double expenseValue = 0.3;
+		
+		for (int i = 1; i <= totalPathCnt; i++) {
+			
+		}
+		
+		return result;
+	}
+
+}
